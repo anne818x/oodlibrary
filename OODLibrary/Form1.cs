d1d7434735e2211e1afe45da670728c08ac67e3a
anne818x <annemarieke-m@hotmail.com>
2016-04-06 11:22:40 +0200

    method for fees

--- OODLibrary/Form1.cs
+++ OODLibrary/Form1.cs
@@ -239,5 +239,124 @@ namespace OODLibrary
             switchBorrow = 1;
             selectItem();
         }
+
+        public double calculateFee(int totalDays, String type, int age)
+        {
+            if (type.Equals("Novel"))
+            {
+                if (totalDays > 21)
+                {
+                    return ((totalDays - 21) * 0.25);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else if (type.Equals("Study Book"))
+            {
+                if (totalDays > 30)
+                {
+                    if (((totalDays - 30) % 7) > 0)
+                    {
+                        return (((totalDays - 30) / 7) + 1);
+                    }
+                    else
+                    {
+                        return ((totalDays - 30) / 7);
+                    }
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else if (type.Equals("Classical"))
+            {
+                double totalCost = 0;
+
+                if (totalDays > 10)
+                {
+                    if (((totalDays - 10) % 7) > 0)
+                    {
+                        totalCost = ((((totalDays - 10) / 7) + 1) * 1.50) + 2;
+                    }
+                    else
+                    {
+                        totalCost = (((totalDays - 10) / 7) * 1.50) + 2;
+                    }
+                }
+                else
+                {
+                    totalCost = 2;
+                }
+
+                if (age > 1 && age < 5)
+                {
+                    return (totalCost * 0.90);
+                }
+                else if (age > 5)
+                {
+                    return (totalCost * 0.50);
+                }
+                else
+                {
+                    return totalCost;
+                }
+            }
+            else if (type.Equals("Popular"))
+            {
+                double totalCost = 0;
+
+                if (totalDays > 10)
+                {
+                    if (((totalDays - 10) % 7) > 0)
+                    {
+                        totalCost = ((((totalDays - 10) / 7) + 1) * 2) + 1;
+                    }
+                    else
+                    {
+                        totalCost = (((totalDays - 10) / 7) * 2) + 1;
+                    }
+                }
+                else
+                {
+                    totalCost = 1;
+                }
+
+                if (age > 1 && age < 5)
+                {
+                    return (totalCost * 0.90);
+                }
+                else if (age > 5)
+                {
+                    return (totalCost * 0.50);
+                }
+                else
+                {
+                    return totalCost;
+                }
+            }
+            else if (type.Equals("A"))
+            {
+                return (totalDays * 2);
+            }
+            else if (type.Equals("B"))
+            {
+                if (totalDays > 3)
+                {
+                    return ((totalDays - 3) + 2);
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        }
     }
+}
 }
