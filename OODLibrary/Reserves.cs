a89dd4250fe8ef46b83e139f858d2a39c315ad2e
Dunstane <Sinistershalun@gmail.com>
2016-04-14 15:26:40 +0200

    merged all get methods into 1 item. Not added the form.cs changes due to others working on it.

--- OODLibrary/Reserves.cs
+++ OODLibrary/Reserves.cs
@@ -1,25 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
-using System.Windows.Forms;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections;
+using System.Windows.Forms;
 
 namespace OODLibrary
 {
-    class Reserves
+    internal class Reserves
     {
-        private string books;
-        private string cds;
-        private string vidtapes;
         private string connection = "datasource=localhost;database=oodlibrary;port=;username=root;password=";
-
-
-        MySqlConnection MyConn;
-
-
+        private MySqlConnection MyConn;
         private string reservations;
         private ArrayList currentlistofid;
 
@@ -41,97 +30,89 @@ namespace OODLibrary
             }
         }
 
-        public String getReservedBooks()
+        public String getReservedItems(string input)//can be changed to type or something
         {
+            //setting up initials
             currentlistofid.Clear();
+            string itemstring = "";
+            string query1 = "";
+            MySqlDataReader MyReader;
+
             MyConn = new MySqlConnection(connection);
-            string query1 = "SELECT IDBook, BookName FROM books WHERE Availability = 'No'";
+
+            switch (input)
+            {
+                case "book":
+                    query1 = "SELECT IDBook, BookName FROM books WHERE Availability = 'No'";
+                    break;
+
+                case "tape":
+                    query1 = "SELECT IDTape, TapeName FROM videotape WHERE Availability = 'No'";
+                    break;
+
+                case "cd":
+                    query1 = "SELECT IDCD, CDName FROM cd WHERE Availability = 'No'";
+                    break;
+            }
+
             MySqlCommand MyCommand = new MySqlCommand(query1, MyConn);
-            MySqlDataReader MyReader;
+
             MyConn.Open();
             MyReader = MyCommand.ExecuteReader();
-            books = null;
-            if (MyReader.HasRows)
+            //  books = null;
+            switch (input)
             {
-                while (MyReader.Read())
-                {
-                    currentlistofid.Add(MyReader["IDBook"].ToString());
-                    books += (MyReader["IDBook"].ToString() + " " + MyReader["BookName"].ToString() + "-");
-                }
+                case "books":
+                    if (MyReader.HasRows)
+                    {
+                        while (MyReader.Read())
+                        {
+                            currentlistofid.Add(MyReader["IDBook"].ToString());
+                            itemstring += (MyReader["IDBook"].ToString() + " " + MyReader["BookName"].ToString() + "-");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No books are borrowed at the moment.");
+                        itemstring = "No books are borrowed at the moment.";
+                    }
+                    break;
+
+                case "tapes":
+                    if (MyReader.HasRows)
+                    {
+                        while (MyReader.Read())
+                        {
+                            currentlistofid.Add(MyReader["IDTape"].ToString());
+                            itemstring += (MyReader["IDTape"].ToString() + " " + MyReader["TapeName"].ToString() + "-");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No video tapes are borrowed at the moment..");
+                        itemstring = "No video tapes are borrowed at the moment.";
+                    }
+                    break;
+
+                case "cds":
+                    if (MyReader.HasRows)
+                    {
+                        while (MyReader.Read())
+                        {
+                            currentlistofid.Add(MyReader["IDCD"].ToString());
+                            itemstring += (MyReader["IDCD"].ToString() + " " + MyReader["CDName"].ToString() + "-");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No cds are borrowed at the moment.");
+                        itemstring = "No cds are borrowed at the moment.";
+                    }
+                    break;
             }
 
-
-            else
-            {
-                MessageBox.Show("No books are borrowed at the moment.");
-                books = "No books are borrowed at the moment.";
-            }
-
-            return books;
+            return itemstring;
         }
-
-        public String getReservedCDs()
-        {
-            currentlistofid.Clear();
-            MyConn = new MySqlConnection(connection);
-            string query1 = "SELECT IDCD, CDName FROM cd WHERE Availability = 'No'";
-            MySqlCommand MyCommand = new MySqlCommand(query1, MyConn);
-            MySqlDataReader MyReader;
-            MyConn.Open();
-            MyReader = MyCommand.ExecuteReader();
-            cds = null;
-
-            if (MyReader.HasRows)
-            {
-                while (MyReader.Read())
-                {
-                    currentlistofid.Add(MyReader["IDCD"].ToString());
-                    cds += (MyReader["IDCD"].ToString() + " " + MyReader["CDName"].ToString() + "-");
-                }
-            }
-            
-
-            else
-            {
-                MessageBox.Show("No cds are borrowed at the moment.");
-                cds = "No cds are borrowed at the moment.";
-            }
-
-            return cds;
-        }
-
-        public String getReservedVidTapes()
-        {
-            currentlistofid.Clear();
-            MySqlConnection MyConn = new MySqlConnection(connection);
-            string query1 = "SELECT IDTape, TapeName FROM videotape WHERE Availability = 'No'";
-            MySqlCommand MyCommand = new MySqlCommand(query1, MyConn);
-            MySqlDataReader MyReader;
-            MyConn.Open();
-            MyReader = MyCommand.ExecuteReader();
-
-            vidtapes = null;
-
-            if (MyReader.HasRows)
-            {
-                while (MyReader.Read())
-                {
-                    currentlistofid.Add(MyReader["IDTape"].ToString());
-                    vidtapes += (MyReader["IDTape"].ToString() + " " + MyReader["TapeName"].ToString() + "-");
-                }
-            }
-
-            else
-            {
-                MessageBox.Show("No video tapes are borrowed at the moment..");
-                vidtapes = "No video tapes are borrowed at the moment.";
-            }
-            return vidtapes;
-
-        }
-
-        
-
 
         public ArrayList giveCurrentSelectionids()
         {
@@ -147,7 +128,7 @@ namespace OODLibrary
         public string getReservations(int id, string type)
         {
             MySqlConnection MyConn = new MySqlConnection(connection);
-            string query = "SELECT * FROM reservations WHERE item_id=" + currentlistofid[id] + " AND type = '"+type+"'";
+            string query = "SELECT * FROM reservations WHERE item_id=" + currentlistofid[id] + " AND type = '" + type + "'";
             MySqlCommand MyCommand = new MySqlCommand(query, MyConn);
             MySqlDataReader MyReader;
             MyConn.Open();
@@ -155,12 +136,10 @@ namespace OODLibrary
             reservations = "";
             while (MyReader.Read())
             {
-                reservations+= (" ID:"+MyReader["reserv_id"].ToString() + " - Member: " + MyReader["c_ID"].ToString() + " - Type: " + MyReader["Type"].ToString() + " - ItemID:  " + MyReader["item_id"].ToString() + " - Reserve Date:  " + MyReader["reservedate"].ToString()+ "?");
-            
+                reservations += (" ID:" + MyReader["reserv_id"].ToString() + " - Member: " + MyReader["c_ID"].ToString() + " - Type: " + MyReader["Type"].ToString() + " - ItemID:  " + MyReader["item_id"].ToString() + " - Reserve Date:  " + MyReader["reservedate"].ToString() + "?");
             }
             return reservations;
         }
-
 
         /// <summary>
         /// places a reservation along with an ID
@@ -168,16 +147,14 @@ namespace OODLibrary
         /// <param name="id"></param>
         /// <param name="c_id"></param>
         /// <param name="type"></param>
-        public void placeReservation(int id, int c_id,  string type)
+        public void placeReservation(int id, int c_id, string type)
         {
             MySqlConnection MyConn = new MySqlConnection(connection);
-            string query = "INSERT INTO reservations VALUES (null," + c_id +","+ "0.5 ," + currentlistofid[id] +","+"'" +type + "'"+ ",CURRENT_TIMESTAMP)"; 
+            string query = "INSERT INTO reservations VALUES (null," + c_id + "," + "0.5 ," + currentlistofid[id] + "," + "'" + type + "'" + ",CURRENT_TIMESTAMP)";
             MySqlCommand MyCommand = new MySqlCommand(query, MyConn);
             MySqlDataReader MyReader;
             MyConn.Open();
             MyReader = MyCommand.ExecuteReader();
         }
-
-
     }
 }
